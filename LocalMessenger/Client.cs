e5821aac1a9b2e744e38e187c93bc47467180a81
Lucah Lucchesi <lucahlucchesi@gmail.com>
2024-03-29 17:19:58 -0500

    created client and server files

---
+++ LocalMessenger/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LocalMessenger
+{
+    public class Client
+    {
+        private TcpClient client;
+        public Client(string serverIP, int port)
+        {
+            client = new TcpClient(serverIP, port);
+        }
+
+        public void SendMessage(string message)
+        {
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                byte[] responseBuffer = new byte[1024];
+                int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+                string response = Encoding.ASCII.GetString(responseBuffer, 0, bytesRead);
+                Console.WriteLine("Server response: " + response);
+
+                stream.Close();
+                client.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ LocalMessenger/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace LocalMessenger
+{
+    public class Server
+    {
+        private TcpListener server;
+
+        public Server(string ip, int port)
+        {
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            server = new TcpListener(ipAddress, port);
+        }
+
+        public void Start()
+        {
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server started. Waiting for connections...");
+
+                while (true)
+                {
+                    // Accepts a new client connection
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Client connected");
+                    HandleClient(client);
+                }
+
+            }catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Recieved: " + message);
+
+                byte[] response = Encoding.ASCII.GetBytes("Message received");
+                stream.Write(response, 0, response.Length);
+            }
+
+            client.Close();
+        }
+        public void Stop()
+        {
+            server.Stop();
+        }
+    }
+}
