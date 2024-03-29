d7980a771ca247448f0251bd08da1acc674295ae
berga4439 <123572908+berga4439@users.noreply.github.com>
2024-03-28 22:19:34 -0500

    Added Main Window
    Added a title screen containing a host and join button

---
+++ LocalMessenger/Home.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace LocalMessenger
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HostBtn = new System.Windows.Forms.Button();
+            this.JoinBtn = new System.Windows.Forms.Button();
+            this.TitleLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HostBtn
+            // 
+            this.HostBtn.Location = new System.Drawing.Point(17, 41);
+            this.HostBtn.Name = "HostBtn";
+            this.HostBtn.Size = new System.Drawing.Size(187, 31);
+            this.HostBtn.TabIndex = 0;
+            this.HostBtn.Text = "Host";
+            this.HostBtn.UseVisualStyleBackColor = true;
+            // 
+            // JoinBtn
+            // 
+            this.JoinBtn.Location = new System.Drawing.Point(17, 78);
+            this.JoinBtn.Name = "JoinBtn";
+            this.JoinBtn.Size = new System.Drawing.Size(187, 29);
+            this.JoinBtn.TabIndex = 1;
+            this.JoinBtn.Text = "Join";
+            this.JoinBtn.UseVisualStyleBackColor = true;
+            // 
+            // TitleLbl
+            // 
+            this.TitleLbl.AutoSize = true;
+            this.TitleLbl.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLbl.Location = new System.Drawing.Point(12, 9);
+            this.TitleLbl.Name = "TitleLbl";
+            this.TitleLbl.Size = new System.Drawing.Size(192, 29);
+            this.TitleLbl.TabIndex = 2;
+            this.TitleLbl.Text = "Local Messenger";
+            this.TitleLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Home
+            // 
+            this.ClientSize = new System.Drawing.Size(223, 123);
+            this.Controls.Add(this.TitleLbl);
+            this.Controls.Add(this.JoinBtn);
+            this.Controls.Add(this.HostBtn);
+            this.Name = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HostBtn;
+        private System.Windows.Forms.Button JoinBtn;
+        private System.Windows.Forms.Label TitleLbl;
+    }
+}
