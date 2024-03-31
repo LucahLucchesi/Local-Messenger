e53714e25c9b95bbbe3891db8a058bd4fea4c396
Lucah Lucchesi <lucahlucchesi@gmail.com>
2024-03-30 20:34:20 -0500

    uncomment client send

--- LocalMessenger/forms/Messenger.cs
+++ LocalMessenger/forms/Messenger.cs
@@ -60,7 +60,7 @@ namespace LocalMessenger
             }
             else
             {
-                //clientRef.sendMsg();
+                clientRef.sendMsg(builtMsg);
             }
             msgInputBox.Clear();
 
