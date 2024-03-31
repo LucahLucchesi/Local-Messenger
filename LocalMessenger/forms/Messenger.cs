ab3d1779d48d23c06c4a1d1afc1976a7a9e54928
berga4439 <123572908+berga4439@users.noreply.github.com>
2024-03-30 20:13:39 -0500

    Moved start server

--- LocalMessenger/forms/Messenger.cs
+++ LocalMessenger/forms/Messenger.cs
@@ -32,6 +32,7 @@ namespace LocalMessenger
             userGroupBox.Text = "Users (1/" + roomSize.ToString() + ")";
             this.Text = this.roomName + " (1/" + roomSize.ToString() + "): " + ipAddr;
             usersList.Items.Add(this.userName);
+            this.serverRef.StartServer();
         }
 
         public Messenger(String roomName, int roomSize, String userName, String ipAddr, Client clientRef) //Client based constructor
